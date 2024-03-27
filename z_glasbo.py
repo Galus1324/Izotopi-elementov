bd4b306a1a22dbbbc577cf9ee6a376bb5ee125c4
Galus1324 <gal.pestotnik@gmail.com>
2024-03-27 20:14:17 +0100

    Update z_glasbo.py

--- z_glasbo.py
+++ z_glasbo.py
@@ -1,1 +1,29 @@
+from turtle import *
+import random
+from playsound import playsound
+
+speed(-1)
+r = 20
+
+playsound("GasGasGas.mp3")
+
+pu()
+goto(-200,450)
+write("prikaz izotopov bakra:", True,"left", ("Arial", 30, "normal")) 
+goto(-400,400)
+for i in range(20):
+    for k in range(20):
+        if random.randint(1,1000)<692:
+            fillcolor("gold")
+        else:
+            fillcolor("silver")
+        begin_fill()
+        pd()
+        circle(r)
+        pu() 
+        fd(2*r) 
+        end_fill()
+    goto(-400, 400-(i+1)*2*r)  
+
+done()
 
