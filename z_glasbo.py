872fe752d1c3c089858dd95c95f587566ad61f8a
Galus1324 <gal.pestotnik@gmail.com>
2024-03-28 17:40:36 +0100

    Update z_glasbo.py

--- z_glasbo.py
+++ z_glasbo.py
@@ -3,7 +3,7 @@ import random
 from pygame import mixer
 
 speed(0)
-r = 3
+r = 20
 screen = Screen()
 screen.delay(0)
 
@@ -12,24 +12,32 @@ mixer.music.load("GasGasGas.mp3")
 mixer.music.play()
 mixer.music.set_pos(50)
 
+list_of_percentages = [492, 277, 40, 185, 6]
 
+colors = ["yellow", "pink", "red", "purple", "green", "gold", "silver", "blue"]
+
+def alg(isotopes):
+    random_num = random.randint(1,1000)
+    for i in range(len(isotopes)):
+        if sum(isotopes[:i+1]) >= random_num:
+            return (colors[i])
+        
 pu()
 goto(-200,450)
 write("prikaz izotopov bakra:", True,"left", ("Arial", 30, "normal")) 
 goto(-400,400)
 for i in range(round(400/r)):
     for k in range(round(400/r)):
-        if random.randint(1,1000)<692:
-            fillcolor("gold")
-        else:
-            fillcolor("silver")
+        fillcolor(alg(list_of_percentages))
         begin_fill()
         pd()
         circle(r)
         pu() 
         fd(2*r) 
         end_fill()
-    goto(-400, 400-(i+1)*2*r)  
+    goto(-400, 400-(i+1)*2*r) 
+
+
 
 done()
 
